fff2cbe59abf3ec2e89ecb9fd8b49c9214e10ce4
habtu <habtassefa@gmail.com>
2019-12-04 22:48:38 -0600

    login and window transition
    - log in transition with

--- src/ui/LoginWindow.java
+++ src/ui/LoginWindow.java
@@ -2,24 +2,9 @@ package ui;
 
 import java.io.IOException;
 
-import business.ControllerInterface;
-import business.LoginException;
-import business.SystemController;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -28,7 +13,7 @@ public class LoginWindow extends Stage implements LibWindow {
 	
 	private boolean isInitialized = false;
 	
-	public boolean isInitialized() {
+	public boolean isInitialized() {	
 		return isInitialized;
 	}
 	public void isInitialized(boolean val) {
@@ -46,6 +31,10 @@ public class LoginWindow extends Stage implements LibWindow {
 	   Parent root = null;
 	   try {
 		root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+		
+		Scene scene = new Scene(root, 800,500);
+		setScene(scene);
+		isInitialized(true);
 	} catch (IOException e) {
 		e.printStackTrace();
 	}
