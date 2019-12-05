52929e157615b38dc16b45506e7d4c29f8e1c15a
habtu <habtassefa@gmail.com>
2019-12-05 17:47:06 -0600

    book copy style fix

--- src/ui/controller/BookController.java
+++ src/ui/controller/BookController.java
@@ -7,18 +7,21 @@ import business.LibrarySystemException;
 import business.SystemController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class BookController {
 	@FXML protected TextField bookISBN; 
-	
+	@FXML protected Label actiontarget; 
 	@FXML protected void handleAddBookCopy(ActionEvent event) throws IOException {
       System.out.println("Add New book");
       ControllerInterface c = new SystemController();
       
       try {
-    	  if(bookISBN.getText().trim().isEmpty())
-    		  	throw new LibrarySystemException("Invalid ISBN"); 
+    	  if(bookISBN.getText().trim().isEmpty()) {
+    		  actiontarget.setVisible(true);
+    		  actiontarget.setText("Invalid ISBN");
+    	  }
     	  else 
     		  c.addBookCopy(bookISBN.getText());
 	} catch (LibrarySystemException e) {
