91884d32e60ecf3d087aa73cea260aed878e992a
tamir <tamir@172.19.144.82>
2019-12-05 17:10:25 -0600

    User displayed on system window

--- src/business/SystemController.java
+++ src/business/SystemController.java
@@ -24,6 +24,7 @@ public class SystemController implements ControllerInterface {
 			throw new LoginException("Password incorrect");
 		}
 		currentAuth = map.get(id).getAuthorization();
+		System.out.println(currentAuth.toString());
 		
 	}
 	@Override
